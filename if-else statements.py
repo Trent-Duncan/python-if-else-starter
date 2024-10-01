bf6e8e0799a7dde4a099b5a8bf178fc3a806e5b9
Trent-Duncan <duncantr131@northwested.org>
2024-10-01 13:51:56 -0400

    update

---
+++ if-else statements.py
@@ -0,0 +1,10 @@
+# Trent Duncan
+# 10/1/24
+# if-else statements
+
+
+color= ['Blue']
+if color == 'green':
+    print('Congrats by shooting the green alien you got 5 points!')
+else:
+    print('COngrats by shooting the Blue alien you gained 50 points!!')
